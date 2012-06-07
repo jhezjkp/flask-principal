8589b3aaecca584cced19801dc2c927aba2aa1ac
jhezjkp <jhezjkp@gmail.com>
2012-06-07 22:59:45 +0800

    添加代码注释

--- flaskext/principal.py
+++ flaskext/principal.py
@@ -8,7 +8,10 @@
     :copyright: (c) 2010-2011 by Ali Afshar.
     :copyright: (c) 2011 by Alfred Hall.
     :license: MIT, see LICENSE for more details.
-
+    
+    注释参考资料：
+        1、stackoverflow.com[http://stackoverflow.com/questions/7050137/\
+           flask-principal-tutorial-auth-authr]
 """
 
 import sys
@@ -33,6 +36,8 @@ Actual name: ``identity-changed``
 Authentication providers should send this signal when authentication has been
 successfully performed. Flask-IdentityContext connects to this signal and
 causes the identity to be saved in the session.
+即用户认证成功时要发送一次该信号并将对应的Identity作为参数传入，对应地，
+Principal将把该Identity信息保存到会话中
 
 For example::
 
@@ -56,6 +61,9 @@ major activities:
     1. Populate the identity object with the necessary authorization
        provisions.
     2. Load any additional user information.
+Identity信息提供方应该在接收到该信息后至少要做以下两个操作：
+    1、给认证成功的Identity植入对应的授权信息
+    2、装载额外的用户信息(这些信息将以identity属性的形式被存储到会话中)
 
 For example::
 
@@ -82,23 +90,23 @@ The ``method`` attribute can be used to look up element 0, and the ``value``
 attribute can be used to look up element 1.
 """
 
-
+#特定用户授权
 UserNeed = partial(Need, 'name')
 UserNeed.__doc__ = """A need with the method preset to `"name"`."""
 
-
+#特定角色授权
 RoleNeed = partial(Need, 'role')
 RoleNeed.__doc__ = """A need with the method preset to `"role"`."""
 
-
+#特定类型授权
 TypeNeed = partial(Need, 'type')
 TypeNeed.__doc__ = """A need with the method preset to `"role"`."""
 
-
+#特定操作授权
 ActionNeed = partial(Need, 'action')
 TypeNeed.__doc__ = """A need with the method preset to `"action"`."""
 
-
+#特定数据项授权
 ItemNeed = namedtuple('RowNeed', ['method', 'value', 'type'])
 """A required item need
 
@@ -114,12 +122,12 @@ reality, the developer is free to choose whatever convention the permissions
 are.
 """
 
-
+#该异常将在用户尝试做一个无权限的操作时抛出
 class PermissionDenied(RuntimeError):
     """Permission denied to the resource
     """
 
-
+#身份
 class Identity(object):
     """Represent the user's identity.
 
@@ -130,12 +138,17 @@ class Identity(object):
     The identity is used to represent the user's identity in the system. This
     object is created on login, or on the start of the request as loaded from
     the user's session.
+    Identity对象在系统中用来代表用户身份。该对象一般中用户登录时创建或在每次
+    发起request请求时从会话中装载。
 
     Once loaded it is sent using the `identity-loaded` signal, and should be
     populated with additional required information.
+    Identity一旦装载后，它就将被名为“identity-loaded”的信号以参数的形式发送出
+    去，并且框架将为它植入额外的必须信息(即授权信息、用户信息等)。
 
     Needs that are provided by this identity should be added to the `provides`
     set after loading.
+    在装载后，对应的Needs信息应该被添加到该对象的一个名为“provides“的set中
     """
     def __init__(self, name, auth_type=''):
         self.name = name
@@ -153,11 +166,11 @@ class Identity(object):
     def can(self, permission):
         """Whether the identity has access to the permission.
 
-        :param permission: The permission to test provision for.
+        :param permission: The permission to test provision for.        
         """
         return permission.allows(self)
 
-
+#匿名用户
 class AnonymousIdentity(Identity):
     """An anonymous identity
 
@@ -167,7 +180,7 @@ class AnonymousIdentity(Identity):
     def __init__(self):
         Identity.__init__(self, 'anon')
 
-
+#认证上下文管理器
 class IdentityContext(object):
     """The context of an identity for a permission.
 
@@ -181,11 +194,14 @@ class IdentityContext(object):
     """
 
     def __init__(self, permission, http_exception=None):
+        #调用permission的require方法时，该上下文将被实例化
         self.permission = permission
         self.http_exception = http_exception
         """The permission of this principal
         """
 
+    #@property将其修饰的函数以“属性”方式调用，调用instance.identity等价于
+    #调用instance.identity()
     @property
     def identity(self):
         """The identity of this principal
@@ -213,8 +229,11 @@ class IdentityContext(object):
     def __enter__(self):
         # check the permission here
         if not self.can():
+            #如果无权限访问
             if self.http_exception:
+                #且指明了无权限时要抛出的异常代码，则调用abort
                 abort(self.http_exception, self.permission)
+            #在未指明无权限时要抛出的异常代码的情况下，抛出PermissionDenied 
             raise PermissionDenied(self.permission)
 
     def __exit__(self, *exc):
@@ -268,6 +287,10 @@ class Permission(object):
         requirements.
 
         :param http_exception: the HTTP exception code (403, 401 etc)
+        由于permission未与identity耦合，因此在这必须调用identity上下文
+        管理器来获取identity信息以判定该操作是否继续或抛出
+        PermissionDenied(未指定http_exception值的情况下)或者调用abort
+        方法并将http_exception值作为参数传入
         """
         return IdentityContext(self, http_exception)
 
@@ -327,6 +350,11 @@ class Permission(object):
         """Whether the identity can access this permission.
 
         :param identity: The identity
+        判定一个identity是否对某permission拥有访问权限要同时满足两个条件：
+            1、permission允许的Need集合(存储于permission对象的needs属性中)与identity被授权的Need集合(存储于
+               identity对象的provides属性中)有交集
+            2、permission拒绝的Need集合(存储于permission对象的excludes属性中)与identity被授权的Need集合(存储于
+               identity对象的provides属性中)没有交集
         """
         if self.needs and not self.needs.intersection(identity.provides):
             return False
@@ -356,6 +384,7 @@ class Denial(Permission):
 
 
 def session_identity_loader():
+    '''从会话中获取身份信息(如果有的话)'''
     if 'identity.name' in session and 'identity.auth_type' in session:
         identity = Identity(session['identity.name'],
                             session['identity.auth_type'])
@@ -363,6 +392,7 @@ def session_identity_loader():
 
 
 def session_identity_saver(identity):
+    '''将身份信息保存到会话中'''
     session['identity.name'] = identity.name
     session['identity.auth_type'] = identity.auth_type
     session.modified = True
@@ -384,10 +414,17 @@ class Principal(object):
             self._init_app(app)
 
     def _init_app(self, app):
+        #将_on_before_request方法注册到app的请求前置处理器，每次请求前都会
+        #执行该方法
         app.before_request(self._on_before_request)
+        #将“identity_changed”信号与_on_identity_changed方法关联，app以参数
+        #形式传入，使得每收到一个“identity_changed”信号时，
+        #_on_identity_changed方法都将关联执行
         identity_changed.connect(self._on_identity_changed, app)
 
         if self.use_sessions:
+            #如果使用会话，则添加一个默认的identity装载器和一个默认的
+            #identity存储器
             self.identity_loader(session_identity_loader)
             self.identity_saver(session_identity_saver)
 
@@ -447,9 +484,14 @@ class Principal(object):
         self.set_identity(identity)
 
     def _on_before_request(self):
+        #该方法在每次请求前都执行一次
+        #为当前请求的全局变量，是flask中的
         g.identity = AnonymousIdentity()
+        #领取取出identity_loaders中的身份装载器(用于从会话中[或其他方式]获取identity)
         for loader in self.identity_loaders:
             identity = loader()
             if identity is not None:
+                #装载到一个有效的identity就持有它
                 self.set_identity(identity)
+                #不再调用后面的装载器
                 return
